58f335a98386e699a26affb1e8a840744be13ec3
Bernard Pagoaga <bernard.pagoaga@gmail.com>
2017-04-17 01:11:44 +0200

    [UPDATE] views

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('test')
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var expressSession = require('express-session');
+
+var app = express();
+
+// getting the assets
+app.use(express.static('public'));
+// templating engine
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res, next) {
+    res.render('index');
+})
+
+app.listen(3000, function() {
+    console.log('running');
+});
